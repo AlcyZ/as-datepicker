f85cc13b4bc92e940ff9a45210a7520defa157ce
toby <tobias_schindler@hotmail.de>
2017-03-18 13:32:51 +0100

    small general adjustments

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,8 @@
 const {mix} = require('laravel-mix');
 
 mix.js('assets/js/datepicker.js', 'dist/js/datepicker.js')
-	.sass('assets/sass/datepicker.scss', 'dist/css/datepicker.css').disableNotifications();
+	.sass('assets/sass/datepicker.scss', 'dist/css/datepicker.css')
+	.options({
+		processCssUrls: false
+	})
+	.disableNotifications();
