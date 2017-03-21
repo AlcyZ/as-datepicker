605bc25de47b34b49fef817a55ef32146be6c98e
toby <tobias_schindler@hotmail.de>
2017-03-21 20:51:37 +0100

    calendar.js improved and extended  - set event listeners

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,15 +1,21 @@
 import LayoutFactory from './layout-factory';
+import Events from './events';
 
 const body = document.getElementsByTagName('body')[0];
+const date = new Date();
 
 let overlay;
 let wrapper;
 let calendar;
 
+/**
+ * Creates the calendar layout.
+ * @private
+ */
 const _createLayout = () => {
 	overlay = LayoutFactory.createOverlay();
 	wrapper = LayoutFactory.createWrapper();
-	calendar = LayoutFactory.createCalendar();
+	calendar = LayoutFactory.createCalendar(date);
 	
 	wrapper.appendChild(calendar);
 	overlay.appendChild(wrapper);
@@ -19,18 +25,22 @@ const _createLayout = () => {
 	setTimeout(() => overlay.style.backgroundColor = 'rgba(0,0,0,0.8)', 1);
 };
 
+/**
+ * Sets event listener for calendar events.
+ * @private
+ */
 const _setEvents = () => {
+	// static event handler
+	Events.close(overlay, wrapper);
 	
-};
-
-const _renderData = () => {
-	
+	// dynamic calls of events
+	document.querySelector('.previous').addEventListener('click', e => Events.previous(e, date));
+	document.querySelector('.next').addEventListener('click', e => Events.next(e, date));
 };
 
 export default {
 	bootstrap: function() {
 		_createLayout();
 		_setEvents();
-		_renderData();
 	}
 }
