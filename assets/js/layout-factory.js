d1c47b1fbd6a827eca9d39f97472929c119b0586
toby <tobias_schindler@hotmail.de>
2017-03-21 20:46:39 +0100

    layout-factory.js improved and extended  - provide function to get unique calendar id  - function to rebuild the calendar  - share some values above functions

--- assets/js/layout-factory.js
+++ assets/js/layout-factory.js
@@ -22,6 +22,23 @@ import calendarData from './calendar-data';
 	}
 })();
 
+// the calendar id is the unique identifier and required for recreation of the calendar
+// function found here: http://stackoverflow.com/questions/6860853/generate-random-string-for-div-id | thx @ Joe
+function guidGenerator() {
+	/**
+	 * @return {string}
+	 */
+	const S4 = () => {
+		return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+	};
+	return (S4() + S4() + '-' + S4() + '-' + S4() + '-' + S4() + '-' + S4() + S4() + S4());
+}
+
+const calendarId = guidGenerator();
+let calendar;
+let month;
+let daysWrapper;
+
 /**
  * Creates the dom of the calendar's month navigation.
  *
@@ -32,11 +49,12 @@ import calendarData from './calendar-data';
 const _createCalendarNav = date => {
 	const nav = document.createElement('div');
 	const previous = document.createElement('div');
-	const month = document.createElement('div');
 	const next = document.createElement('div');
 	
 	const previousIcon = document.createElement('img');
 	const nextIcon = document.createElement('img');
+	
+	month = document.createElement('div');
 	
 	// Todo: Icon path configurable
 	previousIcon.setAttribute('src', '../assets/icons/ic_arrow_back_black_24px.svg');
@@ -49,7 +67,7 @@ const _createCalendarNav = date => {
 	
 	previous.appendChild(previousIcon);
 	next.appendChild(nextIcon);
-	month.innerText = date.getMonthName();
+	month.innerText = date.getMonthName() + ' (' + date.getFullYear() + ')';
 	
 	nav.appendChild(previous);
 	nav.appendChild(month);
@@ -88,14 +106,17 @@ const _createCalendarWeekdays = () => {
  * @private
  */
 const _createDays = date => {
-	// Todo Remove "new Date" and inject as argument later
 	const data = calendarData(date);
-	const daysWrapper = document.createElement('div');
 	let i = 0;
 	let day;
+	let dayInner;
 	let row;
 	
+	daysWrapper = document.createElement('div');
 	daysWrapper.classList.add('days-wrapper');
+	data.length / 7 === 4 ? daysWrapper.classList.add('four') : null;
+	data.length / 7 === 5 ? daysWrapper.classList.add('five') : null;
+	data.length / 7 === 6 ? daysWrapper.classList.add('six') : null;
 	
 	for (; i < data.length; i++) {
 		if (i % 7 === 0) {
@@ -104,7 +125,11 @@ const _createDays = date => {
 		}
 		day = document.createElement('div');
 		day.classList.add('day');
-		day.innerText = data[i].getDate();
+		
+		dayInner = document.createElement('div');
+		dayInner.innerText = data[i].getDate();
+		
+		day.appendChild(dayInner);
 		row.appendChild(day);
 		if (i % 7 === 6) {
 			daysWrapper.appendChild(row);
@@ -122,7 +147,7 @@ export default {
 	 * Creates the black, fixed overlay.
 	 * @returns {Element}
 	 */
-	createOverlay: function() {
+	createOverlay: () => {
 		const overlay = document.createElement('div');
 		overlay.classList.add('overlay');
 		overlay.setAttribute('id', 'as-datetime-picker');
@@ -134,7 +159,7 @@ export default {
 	 * Creates the flexbox wrapper.
 	 * @returns {Element}
 	 */
-	createWrapper: function() {
+	createWrapper: () => {
 		const wrapper = document.createElement('div');
 		wrapper.classList.add('wrapper');
 		
@@ -144,17 +169,30 @@ export default {
 	 * Creates the calendar.
 	 * @returns {Element}
 	 */
-	createCalendar: function() {
-		const today = new Date();
-		today.setMonth(today.getMonth() + 1);
-		
-		const calendar = document.createElement('div');
+	createCalendar: date => {
+		calendar = document.createElement('div');
+		calendar.setAttribute('id', calendarId);
 		calendar.classList.add('calendar');
 		
-		calendar.appendChild(_createCalendarNav(today));
+		calendar.appendChild(_createCalendarNav(date));
 		calendar.appendChild(_createCalendarWeekdays());
-		calendar.appendChild(_createDays(today));
+		calendar.appendChild(_createDays(date));
 		
 		return calendar;
+	},
+	/**
+	 * Rebuilds the dates and month name of the calendar.
+	 * @param {Date} date Current date of calendar.js.
+	 */
+	rebuildCalendar: date => {
+		daysWrapper.remove();
+		calendar.appendChild(_createDays(date));
+		month.innerText = date.getMonthName() + ' (' + date.getFullYear() + ')';
+	},
+	/**
+	 * Returns the id of the calendar.
+	 */
+	getCalendarId: () => {
+		return calendarId;
 	}
 }
