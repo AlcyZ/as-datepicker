73687aea25bf67b48c48fcbb626052c6a5818505
toby <tobias_schindler@hotmail.de>
2017-03-21 20:44:07 +0100

    events.js file provides callbacks for dynamic events and functions to add event listener for static events

---
+++ assets/js/events.js
@@ -0,0 +1,36 @@
+import LayoutFactory from './layout-factory';
+
+export default {
+	/**
+	 * Shows the previous month on the calendar.
+	 * @param {Event} e Click event.
+	 * @param {Date} date Current date of calendar.js, the instance them self gets modified.
+	 */
+	previous: (e, date) => {
+		date.setMonth(date.getMonth() - 1);
+		
+		LayoutFactory.rebuildCalendar(date);
+	},
+	
+	/**
+	 * Shows the next month on the calendar.
+	 * @param {Event} e Click event.
+	 * @param {Date} date Current date of calendar.js, the instance them self gets modified.
+	 */
+	next: (e, date) => {
+		date.setMonth(date.getMonth() + 1);
+		
+		LayoutFactory.rebuildCalendar(date);
+	},
+	
+	/**
+	 * Sets the event handler to close the calendar widget.
+	 * @param overlay
+	 * @param wrapper
+	 */
+	close: (overlay, wrapper) => {
+		overlay.addEventListener('click', e => {
+			e.target === wrapper || e.target === overlay ? overlay.remove() : null;
+		});
+	}
+}
